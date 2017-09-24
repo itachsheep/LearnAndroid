9680b0db808665349b7e8bda80a46b21a8f3f056
taowei <taowei@skyworth.com>
2017-09-24 08:42:28 +0800

    11

--- MvvmDemo/app/src/main/java/com/java/mvvmdemo/base/BaseViewModel.java
+++ MvvmDemo/app/src/main/java/com/java/mvvmdemo/base/BaseViewModel.java
@@ -2,9 +2,12 @@ package com.java.mvvmdemo.base;
 
 import android.app.Activity;
 import android.content.Context;
+import android.databinding.ObservableArrayList;
+import android.databinding.ObservableList;
 
 import com.java.mvvmdemo.BR;
 import com.java.mvvmdemo.R;
+import com.java.mvvmdemo.model.TopItemViewModel;
 
 import me.tatarka.bindingcollectionadapter.ItemView;
 
@@ -18,7 +21,7 @@ public class BaseViewModel implements ViewModel{
 
     // viewModel for recycler header viewPager
     public final ItemView topItemView = ItemView.of(BR.viewModel, R.layout.viewpager_item_top_news);
-    public final
+    public final ObservableList<TopItemViewModel> topItemViewModels = new ObservableArrayList<>();
     public BaseViewModel(Activity activity){
         context = activity;
 //        Messenger.getDefault().register(activity, NewsViewModel.TOKEN_TOP_NEWS_FINISH,
