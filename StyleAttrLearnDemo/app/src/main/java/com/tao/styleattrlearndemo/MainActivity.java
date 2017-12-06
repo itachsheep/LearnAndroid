52173995fd168890a7a5ad629a8fd7021509e024
taowei <taowei@skyworth.com>
2017-12-06 17:32:57 +0800

    总结了style的用法

--- StyleAttrLearnDemo/app/src/main/java/com/tao/styleattrlearndemo/MainActivity.java
+++ StyleAttrLearnDemo/app/src/main/java/com/tao/styleattrlearndemo/MainActivity.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.hardware.input.InputManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +17,22 @@ public class MainActivity extends AppCompatActivity {
 
         InputManager inputManager = (InputManager) getSystemService(Context.INPUT_SERVICE);
 
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+
+
+        return super.dispatchKeyEvent(event);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        try {
+            throw new Exception("printStack");
+        }catch (Exception e){
+            Log.d(TAG,"dispatchTouchEvent--- "+e.getLocalizedMessage(),e);
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
