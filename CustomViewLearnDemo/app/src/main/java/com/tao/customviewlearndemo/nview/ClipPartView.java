c3c77017f073a0e79663235124f1906ec975406d
itachsheep <itachsheep@163.com>
2018-01-16 16:33:45 +0800

    整理代码结构

--- CustomViewLearnDemo/app/src/main/java/com/tao/customviewlearndemo/nview/ClipPartView.java
+++ CustomViewLearnDemo/app/src/main/java/com/tao/customviewlearndemo/nview/ClipPartView.java
@@ -24,10 +24,10 @@ public class ClipPartView extends ImageView {
     private float mRadius;
     private Drawable mDrawable;
     private SectorPartDrawable sectorPartDrawable;
-    private float mProgress;
+
     private float mCenterX;
     private float mCenterY;
-    private OnProgressListener mProgressListener;
+
     public ClipPartView(Context context) {
         this(context,null);
     }
@@ -48,8 +48,7 @@ public class ClipPartView extends ImageView {
     }
 
     public void setProgrss(float progrss){
-        mProgress = progrss;
-        sectorPartDrawable.setPercent(mProgress);
+        sectorPartDrawable.setPercent(progrss);
     }
 
     @Override
@@ -68,82 +67,17 @@ public class ClipPartView extends ImageView {
         super.onDraw(canvas);
     }
 
-
     @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
         Log.i(TAG,"dispatchTouchEvent");
         return super.dispatchTouchEvent(event);
     }
-    public interface OnProgressListener{
-        void onProgressChange(float progress);
-    }
-
-    public void setProgressListener(OnProgressListener listener){
-        mProgressListener = listener;
-    }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         Log.i(TAG,"onTouchEvent");
-        switch (event.getAction()){
-            case MotionEvent.ACTION_DOWN:
-            {
-                float x = event.getX();
-                float y = event.getY();
-                float angle = getAngle(mCenterX, mCenterY, x, y);
-                if(angle >0  && angle <= 30){
-                    mProgress = 30f / 360;
-                }else if(angle > 30 && angle <= 90){
-                    mProgress = 90f / 360;
-                }else if(angle > 90 && angle <= 150){
-                    mProgress = 150f / 360;
-                }else if(angle > 150 && angle <= 240){
-                    mProgress = 240f / 360;
-                }else if(angle > 240 && angle <= 360){
-                    mProgress = 330f /360;
-                }
-                Log.i(TAG,"action down mProgress = "+mProgress+", angle = "+angle);
-                setProgrss(mProgress);
-                if(null != mProgressListener){
-                    mProgressListener.onProgressChange(angle);
-                }
-                break;
-            }
-
-            case MotionEvent.ACTION_UP:
-
-                break;
-            default:
-                break;
-        }
         return super.onTouchEvent(event);
-
     }
 
-    private float getAngle(float centerX, float centerY, float xInView, float yInView){
-        double rotation = 0;
-        double tan = (centerY - yInView)*1.0 / (xInView - centerX);
-        double tmpDegree = Math.atan(tan) / Math.PI * 180;
-        //顺时针算转过的角度的话
-        if (xInView > centerX && yInView < centerY) {  //第一象限  tmpDegree: 90 , 0
-            rotation = 270 - tmpDegree;
-        } else if (xInView < centerX && yInView < centerY) //第二象限 tmpDegree: 0 , -90
-        {
-            rotation = 90 - tmpDegree;
-        } else if (xInView < centerX && yInView > centerY) { //第三象限 tmpDegree: 90, 0
-            rotation = 90 -  tmpDegree;
-        } else if (xInView > centerX && yInView > centerY) { //第四象限 tmpDegree: 0 , -90
-            rotation = 290 - tmpDegree;
-        } else if (xInView == centerX && yInView < centerY) {//
-            rotation = 180;
-        } else if (xInView == centerX && yInView > centerY) {
-            rotation = 0;
-        }else if(yInView == centerY && xInView > centerX){
-            rotation = 270;
-        }else if(yInView == centerY && xInView < centerX) {
-            rotation = 90;
-        }
-//        Log.i(TAG,"getAngle rotation = "+(int)rotation+", tmpDegree = "+tmpDegree);
-        return (float) rotation;
-    }
+
 }
--- CustomViewLearnDemo/app/src/main/java/com/tao/customviewlearndemo/nview/SectorPartRoot.java
+++ CustomViewLearnDemo/app/src/main/java/com/tao/customviewlearndemo/nview/SectorPartRoot.java
@@ -17,7 +17,7 @@ import com.tao.customviewlearndemo.R;
  * Created by SDT14324 on 2018/1/16.
  */
 
-public class SectorPartRoot extends RelativeLayout implements CenterImageView.OnInnerListener, ClipPartView.OnProgressListener {
+public class SectorPartRoot extends RelativeLayout implements CenterImageView.OnInnerListener{
     private String TAG = "SectorPartRoot";
 
     private float mRadius;
@@ -29,6 +29,7 @@ public class SectorPartRoot extends RelativeLayout implements CenterImageView.On
     private CenterImageView centerImageView;
     private TextView tvNum;
     private int mode = 0;
+    private float mProgress;
     public SectorPartRoot(Context context) {
         this(context,null);
     }
@@ -44,12 +45,16 @@ public class SectorPartRoot extends RelativeLayout implements CenterImageView.On
         centerImageView = view.findViewById(R.id.spr_center_iv);
         tvNum = view.findViewById(R.id.spr_tv_num);
         initListener();
+        //初始化时显示开始的进度
+        initClipPartView();
+    }
+
+    private void initClipPartView() {
         clipPartView.setProgrss(30f/360);
     }
 
     private void initListener() {
         centerImageView.setInnerListener(this);
-        clipPartView.setProgressListener(this);
     }
 
     @Override
@@ -64,7 +69,7 @@ public class SectorPartRoot extends RelativeLayout implements CenterImageView.On
         Log.i(TAG,"onMeasure height = "+height+", width = "+width);
     }
 
-   /* @Override
+    @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
         Log.i(TAG,"dispatchTouchEvent ");
         return super.dispatchTouchEvent(ev);
@@ -74,34 +79,56 @@ public class SectorPartRoot extends RelativeLayout implements CenterImageView.On
     public boolean onInterceptTouchEvent(MotionEvent ev) {
         Log.i(TAG,"onInterceptTouchEvent ");
         return super.onInterceptTouchEvent(ev);
-    }*/
+    }
 
-    /*@Override
+    @Override
     public boolean onTouchEvent(MotionEvent event) {
-        Log.i(TAG,"onTouchEvent ");
+        Log.i(TAG,"onTouchEvent");
         switch (event.getAction()){
             case MotionEvent.ACTION_DOWN:
             {
                 float x = event.getX();
                 float y = event.getY();
-                double r = Math.pow((Math.pow(x - mCenterX, 2) + Math.pow(y - mCenterY, 2)), 0.5);
-                Log.i(TAG,"r = "+r+", mInnerRadius = "+mInnerRadius);
-                if(r < mInnerRadius){
-                    showOrHide();
-                   return true;
+                float angle = getAngle(mCenterX, mCenterY, x, y);
+                setAngleAndMode(angle);
+                Log.i(TAG,"action down mProgress = "+mProgress+", angle = "+angle);
+                if(clipPartView != null){
+                    clipPartView.setProgrss(mProgress);
                 }
+                updateTvNum();
                 break;
             }
-
+            case MotionEvent.ACTION_UP:
+                break;
+            default:
+                break;
         }
         return super.onTouchEvent(event);
-    }*/
+    }
+
+    private void setAngleAndMode(float angle) {
+        if(angle >0  && angle <= 30){
+            mProgress = 30f / 360;
+            mode = 0;
+        }else if(angle > 30 && angle <= 90){
+            mProgress = 90f / 360;
+            mode = 15;
+        }else if(angle > 90 && angle <= 150){
+            mProgress = 150f / 360;
+            mode = 30;
+        }else if(angle > 150 && angle <= 240){
+            mProgress = 240f / 360;
+            mode = 45;
+        }else if(angle > 240 && angle <= 360){
+            mProgress = 330f /360;
+            mode = 60;
+        }
+    }
 
     private void showClipPartView(){
         if (clipPartView.getVisibility() == View.INVISIBLE){
             clipPartView.setVisibility(View.VISIBLE);
         }
-
     }
     public void hideClipPartView(){
         if(clipPartView.getVisibility() == View.VISIBLE){
@@ -120,25 +147,34 @@ public class SectorPartRoot extends RelativeLayout implements CenterImageView.On
         showClipPartView();
     }
 
-    @Override
-    public void onProgressChange(float angle) {
-        //1，更新数值
-        updateTvNum(angle);
-        //2，角度变换
+    private void updateTvNum( ) {
+        tvNum.setText(mode + "s");
     }
 
-    private void updateTvNum(float angle) {
-        if(angle >0  && angle <= 30){
-            mode = 0;
-        }else if(angle > 30 && angle <= 90){
-            mode = 15;
-        }else if(angle > 90 && angle <= 150){
-            mode = 30;
-        }else if(angle > 150 && angle <= 240){
-            mode = 45;
-        }else if(angle > 240 && angle <= 360){
-            mode = 60;
+    private float getAngle(float centerX, float centerY, float xInView, float yInView){
+        double rotation = 0;
+        double tan = (centerY - yInView)*1.0 / (xInView - centerX);
+        double tmpDegree = Math.atan(tan) / Math.PI * 180;
+        //顺时针算转过的角度的话
+        if (xInView > centerX && yInView < centerY) {  //第一象限  tmpDegree: 90 , 0
+            rotation = 270 - tmpDegree;
+        } else if (xInView < centerX && yInView < centerY) //第二象限 tmpDegree: 0 , -90
+        {
+            rotation = 90 - tmpDegree;
+        } else if (xInView < centerX && yInView > centerY) { //第三象限 tmpDegree: 90, 0
+            rotation = 90 -  tmpDegree;
+        } else if (xInView > centerX && yInView > centerY) { //第四象限 tmpDegree: 0 , -90
+            rotation = 290 - tmpDegree;
+        } else if (xInView == centerX && yInView < centerY) {//
+            rotation = 180;
+        } else if (xInView == centerX && yInView > centerY) {
+            rotation = 0;
+        }else if(yInView == centerY && xInView > centerX){
+            rotation = 270;
+        }else if(yInView == centerY && xInView < centerX) {
+            rotation = 90;
         }
-        tvNum.setText(mode + "s");
+//        Log.i(TAG,"getAngle rotation = "+(int)rotation+", tmpDegree = "+tmpDegree);
+        return (float) rotation;
     }
 }
