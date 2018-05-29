0422322190a58f4be85f7f2e5bc71afa6ae2dbdc
itachsheep <itachsheep@163.com>
2018-05-29 17:34:46 +0800

    实现分屏显示功能

---
+++ skyworth/Nu409_SplitScreen-t/app/src/main/java/com/tao/nu409_splitscreen/fragment/MainFullFragment.java
@@ -0,0 +1,14 @@
+package com.tao.nu409_splitscreen.fragment;
+
+import com.tao.nu409_splitscreen.R;
+
+/**
+ * Created by SDT14324 on 2018/5/29.
+ */
+
+public class MainFullFragment extends BaseFragment {
+    @Override
+    public int getLayoutRes() {
+        return R.layout.frg_main_full;
+    }
+}
---
+++ skyworth/Nu409_SplitScreen-t/app/src/main/java/com/tao/nu409_splitscreen/util/L.java
@@ -0,0 +1,14 @@
+package com.tao.nu409_splitscreen.util;
+
+import android.util.Log;
+
+/**
+ * Created by SDT14324 on 2018/5/29.
+ */
+
+public class L {
+    private static String TAG = "Spl";
+    public static void i(String tag,String mes){
+        Log.i(TAG,tag+"."+mes);
+    }
+}
