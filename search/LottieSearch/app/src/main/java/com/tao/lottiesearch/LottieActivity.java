c1c749b2ea71a5a94707fc3f62bd0299696a0483
itachsheep <itachsheep@163.com>
2018-08-05 10:52:31 +0800

    使用airbnb lotttie 实现动画效果

---
+++ search/LottieSearch/app/src/main/java/com/tao/lottiesearch/LottieActivity.java
@@ -0,0 +1,27 @@
+package com.tao.lottiesearch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class LottieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lottie);
+
+        LottieAnimationView animationOriginView = (LottieAnimationView) findViewById(R.id.animation_origin_view);
+//        animationView.setAnimation("hello-world.json");
+        animationOriginView.loop(true);
+        animationOriginView.playAnimation();
+
+
+        LottieAnimationView animationView = (LottieAnimationView) findViewById(R.id.animation_view);
+//        animationView.setAnimation("hello-world.json");
+        animationView.loop(true);
+        animationView.playAnimation();
+
+    }
+}
