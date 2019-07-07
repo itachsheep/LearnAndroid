fe6a04ca7c217600cf130c03da11edbc2a6484e2
itachsheep <itachsheep@163.com>
2019-07-07 15:25:14 +0800

    11

--- flutter_app/lib/RandomWordsState.dart
+++ flutter_app/lib/RandomWordsState.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new RandomWordsState();
+  }
+}
+
 class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
   final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+  final Set<WordPair> _saved = new Set<WordPair>;
 
   Widget _buildSuggestions() {
     return new  ListView.builder(
@@ -21,29 +30,67 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair){
+    final bool alreadySaved = _saved.contains(pair);
     return new ListTile(
       title: new Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+
+      onTap: (){
+        setState(() {
+          if(alreadySaved){
+            _saved.remove(pair);
+          }else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    //final WordPair wordPair = new WordPair.random();
+    //return new Text(wordPair.asPascalCase);
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Scaffold title: Startup Name Generator"),
+        actions: <Widget>[
+          new IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved)
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
 
-    final WordPair wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
+  void _pushSaved(){
+    Navigator.of(context).push(
+      // ignore: expected_token
+      new MaterialPageRoute<void>(builder: (BuildContext context) {
+        final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair){
+              return new ListTile(title: new Text(
+                pair.asPascalCase,
+                style: _biggerFont,
+              ),
+              );
+            }
+        );
+        final List<Widget> divided = ListTile.divideTiles(
+          context: context,
+          tiles: tiles
+        ).toList();
+      },
+      ),
+    );
   }
 }
 
 
 
 
-class RandomWords extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return new RandomWordsState();
-  }
-
-}
--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -4,21 +4,26 @@ import 'package:flutter_app/RandomWordsState.dart';
 void main()=> runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    return new MaterialApp(
-      title: 'This is title',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: const Text("this appbar title"),
-        ),
-        body: Center(
-          //child: const Text("Tao Love Mao"),
-          //child: new Text(wordPair.asPascalCase),
-          child: RandomWords(),
-        ),
-      ),
-    );
+//  Widget build(BuildContext context) {
+//    final wordPair = new WordPair.random();
+//    return new MaterialApp(
+//      title: 'This is title',
+//      home: new Scaffold(
+//        appBar: new AppBar(
+//          title: const Text("this appbar title"),
+//        ),
+//        body: Center(
+//          //child: const Text("Tao Love Mao"),
+//          //child: new Text(wordPair.asPascalCase),
+//          child: RandomWords(),
+//        ),
+//      ),
+//    );
+//  }
+    Widget build(BuildContext context) {
+      return new MaterialApp(
+        title: "MaterialApp title：Startup Name Generator",
+        home:new RandomWords(),
+      );
   }
-
 }
