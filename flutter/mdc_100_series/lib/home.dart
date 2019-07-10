90b06e4c8684df56cdad7e9fbb5226970677a39d
itachsheep <itachsheep@163.com>
2019-07-10 08:12:52 +0800

    mdc-2 , create appbar,iconbutton, icons,onpress

--- flutter/mdc_100_series/lib/home.dart
+++ flutter/mdc_100_series/lib/home.dart
@@ -22,10 +22,43 @@ class HomePage extends StatelessWidget {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
     return Scaffold(
-      appBar: new AppBar(
-        title: const Text("home title"),
-      ),
       // TODO: Add app bar (102)
+      appBar: AppBar(
+        //为 AppBar 的 leading 字段设置一个 IconButton 。
+        // （需要将其放在 title 字段前面，因为它们遵循从头至尾 leading-to-trailing 的先后顺序）
+        leading: IconButton(
+            icon: Icon(
+              Icons.menu,
+              semanticLabel: 'menu',
+            ),
+            onPressed: (){
+              print('Menu Button');
+            }
+        ),
+        title: const Text("SHRINE"),
+        // TODO: Add trailing buttons (102)
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+            onPressed: (){
+              print('Search Button');
+            },
+          ),
+
+          IconButton(
+            icon: Icon(
+              Icons.tune,
+              semanticLabel: 'filter',
+            ),
+            onPressed: (){
+              print('Filter Button');
+            },
+          )
+        ],
+      ),
       // TODO: Add a grid view (102)
       body: Center(
         child: Text('You did it!'),
