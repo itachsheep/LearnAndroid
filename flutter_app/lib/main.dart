3f5886a3074e98ecdddc129cf3633bec52aa52d2
itachsheep <itachsheep@163.com>
2019-07-13 09:15:59 +0800

    flutter layout learn

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,25 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:flutter_app/RandomWordsState.dart';
-void main()=> runApp(new MyApp());
+
+import 'LearnLayout.dart';
+//void main()=> runApp(new MyApp());
+void main() {
+//  runApp(new MyApp());
+  runApp(new FlutterLayout());
+}
 class MyApp extends StatelessWidget {
   @override
-//  Widget build(BuildContext context) {
-//    final wordPair = new WordPair.random();
-//    return new MaterialApp(
-//      title: 'This is title',
-//      home: new Scaffold(
-//        appBar: new AppBar(
-//          title: const Text("this appbar title"),
-//        ),
-//        body: Center(
-//          //child: const Text("Tao Love Mao"),
-//          //child: new Text(wordPair.asPascalCase),
-//          child: RandomWords(),
-//        ),
-//      ),
-//    );
-//  }
+
     Widget build(BuildContext context) {
       return new MaterialApp(
         title: "MaterialApp title：Startup Name Generator",
