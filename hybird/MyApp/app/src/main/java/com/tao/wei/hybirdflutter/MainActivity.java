180f68928d8426c1998f6ca48bf9f288f69bfcc9
itachsheep <itachsheep@163.com>
2019-07-17 08:38:21 +0800

    create hybird project

--- hybird/MyApp/app/src/main/java/com/tao/wei/hybirdflutter/MainActivity.java
+++ hybird/MyApp/app/src/main/java/com/tao/wei/hybirdflutter/MainActivity.java
@@ -2,6 +2,10 @@ package com.tao.wei.hybirdflutter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import io.flutter.facade.Flutter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View flutterView = Flutter.createView(
+                        MainActivity.this,
+                        getLifecycle(),
+                        "route1"
+                );
+                FrameLayout.LayoutParams layout = new FrameLayout.LayoutParams(600, 800);
+                layout.leftMargin = 100;
+                layout.topMargin = 200;
+                addContentView(flutterView, layout);
+            }
+        });
     }
 }
